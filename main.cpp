fc41c2a91d1e65307f40038065b6cf2cd55a5c01
Anaerob <linus.nor@gmail.com>
2017-10-05 12:46:27 +0200

    added class Game

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include "Constants.h"
+#include "Game.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(c_wSX, c_wSY), "Pokemon Nuzlocke Client");
 	window.setVerticalSyncEnabled(true);
 	window.setKeyRepeatEnabled(false);
+
+	Game game;
 
 	while (window.isOpen())
 	{
@@ -16,8 +19,18 @@ int main()
 			case sf::Event::Closed:
 				window.close();
 				break;
+			case sf::Event::KeyPressed:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Escape:
+					window.close();
+					break;
+				}
+				break;
 			}
+
 		window.clear(sf::Color::White);
+		game.draw(window);
 		window.display();
 	}
 	return 0;
