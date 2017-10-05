7ab16edc9dd2ddb11e3bf6ed1914b03788804fb4
Anaerob <linus.nor@gmail.com>
2017-10-05 12:09:25 +0200

    simple window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
-#include <string>
+#include <SFML/Graphics.hpp>
+
+const int c_wSX = 960;
+const int c_wSY = 540;
 
 int main()
 {
-	std::cout << "hello world" << std::endl;
-  
-  return 0;
+	sf::RenderWindow window(sf::VideoMode(c_wSX, c_wSY), "Pokemon Nuzlocke Client");
+	window.setVerticalSyncEnabled(true);
+	window.setKeyRepeatEnabled(false);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		window.clear(sf::Color::White);
+		window.display();
+	}
+	return 0;
 }
