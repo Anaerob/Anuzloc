5cb198f13686e6286354c06e44caaf9ff4381aaa
Anaerob <linus.nor@gmail.com>
2017-10-11 17:03:25 +0200

    added class Move

---
+++ Headers/Move.h
@@ -0,0 +1,22 @@
+#ifndef MOVE_H
+#define MOVE_H
+#include <string>
+#include "Constants.h"
+
+class Move
+{
+	int m_index;
+	std::string m_name;
+	int m_stats[3];
+
+public:
+	Move(int index = 0);
+
+	int getIndex() { return m_index; }
+	std::string getName() { return m_name; }
+	int getStat(int i) { return m_stats[i]; }
+
+	void generateStats();
+};
+
+#endif
---
+++ Sources/Move.cpp
@@ -0,0 +1,17 @@
+#include "Move.h"
+
+Move::Move(int index) : m_index{ index }
+{
+	if (index != 0)
+	{
+		m_name = c_moveNames[index - 1];
+		generateStats();
+	}
+		
+}
+
+void Move::generateStats()
+{
+	for (int i = 0; i < 3; ++i)
+		m_stats[i] = c_moveBaseStats[m_index - 1][i];
+}
