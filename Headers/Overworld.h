18aad9eeb348ddfbcfe4f29d58ce66ea2a87018b
Anaerob <linus.nor@gmail.com>
2017-10-11 16:12:56 +0200

    merged OverworldView into Overworld, needs cleaning

--- Headers/Overworld.h
+++ Headers/Overworld.h
@@ -9,7 +9,7 @@
 #include "Constants.h"
 #include "Event.h"
 //#include "OverworldMenu.h"
-#include "OverworldView.h"
+//#include "OverworldView.h"
 #include "Player.h"
 #include "TextBox.h"
 #include "Trainer.h"
@@ -19,7 +19,6 @@ class Overworld
 {
 	//Battle& m_battle;
 	Event& m_event;
-	//bool m_inEvent;
 	//OverworldMenu m_menu;
 	std::map<int, Trainer> m_NPCs;
 	Player& m_player;
@@ -28,19 +27,29 @@ class Overworld
 	int m_sizeY;
 	std::map<int, std::string> m_strings;
 	TextBox& m_textBox;
-	//int *m_tileMap;
 	std::vector<int> m_tileMap;
-	//int *m_triggerMap;
-	OverworldView m_view;
+	//OverworldView m_view;
 	White m_whiteMenu;
 	White m_whiteTextBox;
 	std::string m_world;
+
+	bool m_edgeX;
+	bool m_edgeY;
+	int m_offsetX;
+	int m_offsetY;
+	sf::VertexArray m_tiles;
+	sf::Texture m_tileSet;
 
 public:
 	Overworld(Event& event, Player& player, TextBox& textBox);// Player& player, Battle& battle, TextBox& textBox);
 
 	int getTile(int x, int y) { return m_tileMap[x +	y * (m_sizeX + 2)] / 1000; }
 	int getTrigger(int x, int y) { return m_tileMap[x + y * (m_sizeX + 2)] % 1000; }
+
+	bool getEdgeX() { return m_edgeX; }
+	bool getEdgeY() { return m_edgeY; }
+	int getOffsetX() { return m_offsetX; }
+	int getOffsetY() { return m_offsetY; }
 
 	//void deallocateTriggerMap();
 	//void deallocateTileMap();
@@ -55,6 +64,11 @@ public:
 	//void readTrainers(std::string trainersPath);
 	//void readTriggerMap(std::string triggerMapPath);
 	void readWorld();
+
+	void calculateOffsetX(int overworldSizeX);
+	void calculateOffsetY(int overworldSizeY);
+	void drawV(sf::RenderWindow& window,
+		std::vector<int> overworld, int overworldSizeX, int overworldSizeY);
 };
 
 #endif
--- Sources/Overworld.cpp
+++ Sources/Overworld.cpp
@@ -5,24 +5,28 @@ Overworld::Overworld(Event& event, Player& player, TextBox& textBox)
 	m_event{ event },
 	//m_inEvent{ false },
 	//m_menu{ player },
-	m_view{ player },
+	//m_view{ player },
 	m_player{ player },
 	m_textBox{ textBox },
 	m_whiteMenu{ cf_mPX, cf_mPY, cf_mSX, cf_mSY },
 	m_whiteTextBox{ cf_tBPX, cf_tBPY, cf_tBSX, cf_tBSY },
 	m_world{ "route1" }
 {
+	m_tiles.setPrimitiveType(sf::Quads);
+	m_tiles.resize((c_vSX / c_tS + 2) * (c_vSY / c_tS + 2) * 4);
+	m_tileSet.loadFromFile("Resources/Tiles/Overworld.png");
+
 	readWorld();
 	m_event.setStrings(m_strings);
 }
 
 void Overworld::draw(sf::RenderWindow& window)
 {
-	m_view.draw(window, m_tileMap, m_sizeX, m_sizeY);
+	drawV(window, m_tileMap, m_sizeX, m_sizeY);
 
 	for (size_t i = 1; i < m_NPCs.size() + 1; ++i)
-		m_NPCs.find(i)->second.draw(window, m_view.getEdgeX(), m_view.getEdgeY(),
-			m_view.getOffsetX(), m_view.getOffsetY(),
+		m_NPCs.find(i)->second.draw(window, getEdgeX(), getEdgeY(),
+			getOffsetX(), getOffsetY(),
 			m_player.getPartialX(), m_player.getPartialY());
 
 	m_whiteMenu.draw(window);
@@ -217,4 +221,103 @@ void Overworld::readWorld()
 	std::string trainersPath = worldPath;
 	trainersPath.append("Trainers.dat");
 	readTrainers(trainersPath);*/
+}
+
+void Overworld::calculateOffsetX(int overworldSizeX)
+{
+	int half = (c_vSX / c_tS - 1) / 2;
+	if (m_player.getX() <= half + 1)
+	{
+		m_offsetX = 0;
+		m_edgeX = m_player.getX() < half + 1 || m_player.getDirection() == DIRECTION_LEFT;
+	}
+	else if (m_player.getX() >= overworldSizeX - half)
+	{
+		m_offsetX = overworldSizeX - c_vSX / c_tS;
+		m_edgeX = m_player.getX() > overworldSizeX - half || m_player.getDirection() == DIRECTION_RIGHT;
+	}
+	else
+	{
+		m_offsetX = m_player.getX() - half - 1;
+		m_edgeX = false;
+	}
+}
+void Overworld::calculateOffsetY(int overworldSizeY)
+{
+	int half = (c_vSY / c_tS - 1) / 2;
+	if (m_player.getY() <= half + 1)
+	{
+		m_offsetY = 0;
+		m_edgeY = m_player.getY() < half + 1 || m_player.getDirection() == DIRECTION_UP;
+	}
+	else if (m_player.getY() >= overworldSizeY - half)
+	{
+		m_offsetY = overworldSizeY - c_vSY / c_tS;
+		m_edgeY = m_player.getY() > overworldSizeY - half || m_player.getDirection() == DIRECTION_DOWN;
+	}
+	else
+	{
+		m_offsetY = m_player.getY() - half - 1;
+		m_edgeY = false;
+	}
+}
+void Overworld::drawV(sf::RenderWindow& window,
+	std::vector<int> overworld, int overworldSizeX, int overworldSizeY)
+{
+	calculateOffsetX(overworldSizeX);
+	calculateOffsetY(overworldSizeY);
+
+	for (int y = 0; y < c_vSY / c_tS + 2; ++y)
+		for (int x = 0; x < c_vSX / c_tS + 2; ++x)
+		{
+			float f_tileNumber;
+
+			float f_x = (float)x;
+			float f_y = (float)y;
+			float f_pX;
+			float f_pY;
+
+			if (m_edgeX)
+				f_pX = 0.0f;
+			else
+				f_pX = (float)(m_player.getPartialX() % c_tS);
+			if (m_edgeY)
+			{
+				f_tileNumber = (float)(overworld[(x + m_offsetX +
+					m_player.getPartialX() / c_tS) + (y + m_offsetY +
+						m_player.getPartialY() / (2 * c_tS)) * (overworldSizeX + 2)] / 1000);
+				f_pY = 0.0f;
+			}
+			else
+			{
+				f_tileNumber = (float)(overworld[(x + m_offsetX +
+					m_player.getPartialX() / c_tS) + (y + m_offsetY +
+						m_player.getPartialY() / c_tS) * (overworldSizeX + 2)] / 1000);
+				f_pY = (float)(m_player.getPartialY() % c_tS);
+			}
+
+			sf::Vertex* quad = &m_tiles[(x + y * (c_vSX / c_tS + 2)) * 4];
+
+			quad[0].position = sf::Vector2f((f_x - 1) * cf_tS - f_pX,
+				(f_y - 1) * cf_tS - f_pY);
+			quad[1].position = sf::Vector2f(f_x * cf_tS - f_pX,
+				(f_y - 1) * cf_tS - f_pY);
+			quad[2].position = sf::Vector2f(f_x * cf_tS - f_pX,
+				f_y * cf_tS - f_pY);
+			quad[3].position = sf::Vector2f((f_x - 1) * cf_tS - f_pX,
+				f_y * cf_tS - f_pY);
+
+			quad[0].texCoords = sf::Vector2f(f_tileNumber * cf_tS,
+				0.0f);
+			quad[1].texCoords = sf::Vector2f((f_tileNumber + 1) * cf_tS,
+				0.0f);
+			quad[2].texCoords = sf::Vector2f((f_tileNumber + 1) * cf_tS,
+				cf_tS);
+			quad[3].texCoords = sf::Vector2f(f_tileNumber * cf_tS,
+				cf_tS);
+		}
+
+	window.draw(m_tiles, &m_tileSet);
+
+	m_player.draw(window, m_edgeX, m_edgeY, m_offsetX, m_offsetY);
 }
