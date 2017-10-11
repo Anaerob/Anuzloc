7ea715e2816f19c3ecf5b3d069ae3c2836e9e054
Anaerob <linus.nor@gmail.com>
2017-10-11 21:02:44 +0200

    added the Matlab script that makes world.dat from .png

---
+++ Matlab/makeWorld.m
@@ -0,0 +1,32 @@
+%% TRANSFORMS PNG TO DAT
+clear all;
+clc;
+
+world = 'route1';
+png = '.png';
+dat = '.dat';
+
+input = imread(strcat(world, png));
+sizeX = size(input, 2);
+sizeY = size(input, 1);
+
+output = zeros(sizeY, sizeX);
+for x = 1:sizeX
+  for y = 1:sizeY
+    if permute(input(y, x, :), [1 3 2]) == [0 0 0]
+      output(y, x) = 0;
+    elseif permute(input(y, x, :), [1 3 2]) == [255 255 255]
+      output(y, x) = 1;
+    elseif permute(input(y, x, :), [1 3 2]) == [0 200 0]
+      output(y, x) = 2;
+    elseif permute(input(y, x, :), [1 3 2]) == [255 0 0]
+      output(y, x) = 3;
+    elseif permute(input(y, x, :), [1 3 2]) == [150 100 50]
+      output(y, x) = 4;
+    end
+  end
+end
+
+dlmwrite(strcat(world, dat), [sizeX, sizeY], 'delimiter', ' ');
+dlmwrite(strcat(world, dat), ' ', '-append', 'delimiter', ' ');
+dlmwrite(strcat(world, dat), output, '-append', 'delimiter', ' ');
